1001a238316fcae37d451ebe2a479d4cfb137366
pferv <pferv77@gmail.com>
2015-11-07 00:21:13 +0100

    Entity+Player classes

---
+++ source/Entities/Entity.hx
@@ -0,0 +1,27 @@
+package;
+
+import flixel.FlxSprite;
+
+
+class Entity extends FlxSprite
+{
+	public var world : World;
+
+	public function new(X : Float, Y : Float, World : World)
+	{
+		super(X,Y);
+
+		world = World;
+	}
+
+	override public function update() : Void
+	{
+		super.update();
+	}
+
+	override public function draw() :  Void
+	{
+		super.draw();
+	}
+
+}
